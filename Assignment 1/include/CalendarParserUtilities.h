1106cf31a94ab92ae6182fff53e4d66682f1f806
Nicholas Nolan <nknolan@users.noreply.github.com>
2019-04-14 15:46:01 -0400

    Update CalendarParserUtilities.h

--- Assignment 1/include/CalendarParserUtilities.h
+++ Assignment 1/include/CalendarParserUtilities.h
@@ -11,12 +11,6 @@
 
 #include "CalendarParser.h"
 
-/** Function to remove the trailing /r/n from a string
- *@pre str exists, is not null
- *@post str trailing characters have been removed
- *@return none
- *@param str
-**/
 void chop(char * str);
 
 void parseDateTime(DateTime dt, char * body);
